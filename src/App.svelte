5ac71feb5a4af5d1a01fe691cde6345d5e7af119
James Coyle <jamesrichardcoyle@gmail.com>
2021-01-30 17:39:33 +0000

    Add border color property

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,22 @@
 <script>
-	import Download from "./components/Download.svelte";
+	import Download from './components/Download.svelte'
 
-	let downloads = [];
+	let downloads = []
 
 	function updateDownloadList() {
 		chrome.downloads.search({}, (d) => {
-			downloads = d.filter((d) => d.filename && d.incognito == false);
-			console.log(downloads);
-		});
+			downloads = d.filter((d) => d.filename && d.incognito == false)
+			console.log(downloads)
+		})
 	}
 
 	function openDownloadsTab() {
-		const tab = { url: "chrome://downloads" };
-		chrome.tabs.create(tab);
+		const tab = { url: 'chrome://downloads' }
+		chrome.tabs.create(tab)
 	}
 
 	// set icon to match browser theme
-	const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
-		? "dark"
-		: window.matchMedia("(prefers-color-scheme: light)").matches
-		? "light"
-		: "default";
+	const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'default'
 
 	chrome.browserAction.setIcon({
 		path: {
@@ -29,21 +25,21 @@
 			32: `icons/${theme}/icon-32.png`,
 			48: `icons/${theme}/icon-48.png`,
 		},
-	});
+	})
 
 	// allow background to detect when popup open
-	chrome.runtime.connect({ name: "popup" });
+	chrome.runtime.connect({ name: 'popup' })
 
 	// update the download list when events fire
-	chrome.downloads.onCreated.addListener(updateDownloadList);
-	chrome.downloads.onChanged.addListener(updateDownloadList);
-	chrome.downloads.onErased.addListener(updateDownloadList);
+	chrome.downloads.onCreated.addListener(updateDownloadList)
+	chrome.downloads.onChanged.addListener(updateDownloadList)
+	chrome.downloads.onErased.addListener(updateDownloadList)
 
 	// poll for list changes
-	setInterval(updateDownloadList, 500);
+	setInterval(updateDownloadList, 500)
 
 	// populate downloads on first load
-	updateDownloadList();
+	updateDownloadList()
 </script>
 
 <style>
@@ -76,7 +72,7 @@
 		text-align: left;
 		background-color: transparent;
 		border: none;
-		border-top: 1px solid hsla(0, 0%, 100%, 0.15);
+		border-top: 1px solid var(--border-color);
 		outline: none;
 	}
 
@@ -106,11 +102,7 @@
 			<Download {download} />
 		{:else}
 			<div class="center">
-				<img
-					width="160"
-					height="160"
-					src="images/no-downloads.svg"
-					alt="" />
+				<img width="160" height="160" src="images/no-downloads.svg" alt="" />
 				<p>Files you download appear here</p>
 			</div>
 		{/each}
@@ -118,9 +110,7 @@
 
 	<button class="button" on:click={openDownloadsTab}>
 		<svg width="16" height="16" viewBox="0 0 24 24">
-			<path
-				d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z"
-				fill="currentColor" />
+			<path d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" fill="currentColor" />
 		</svg>
 		<span>Manage downloads</span>
 	</button>
